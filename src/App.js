2ee2b341d444fbc7c1cfb3fd9099c28274f91aed
Dilandds <e17379@eng.pdn.ac.lk>
2023-03-08 09:47:30 +0600

    added expenses component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import ExpenseItem from "./components/ExpenseItem";
-
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses></Expenses>
     </div>
   );
 }
